cfff3488a2b45b2c86d106a47f8d1d10ea1ec65b
Greg <33642294+TestForEcho12@users.noreply.github.com>
2022-01-06 15:30:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name='femaptools',
+      version='0.1',
+      author='Greg Baker',
+      author_email='greg.baker@jhuapl.edu',
+      license='MIT',
+      description='A way to interface with the Femap API and exicute common commands'
+      )
